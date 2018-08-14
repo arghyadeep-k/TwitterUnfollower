e6a8617d81bcdf96644042c4bfaa5b5d673caa79
Arghyadeep <arghyadeep.k@gmail.com>
2018-08-14 16:06:44 +0000

    Added followers list fetch functionality

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var Twitter = require('twitter');
 var config = require('./config.js');
+var ArrayList = require('arraylist');
 
 var T = new Twitter(config);
 
-//var list = new ArrayList();
+var friendsList = new ArrayList();
+var followersList = new ArrayList();
+var unfollowersList = new ArrayList();
 
 //Obtaining friends list
 T.get('friends/ids',function(err,data,response){
@@ -12,8 +15,8 @@ T.get('friends/ids',function(err,data,response){
         console.log(data.ids.length);
         //Insert code
         for(let i=0;i<data.ids.length;i++)
-            //list.add(data.users[i].name);
-            console.log(data.ids[i]);
+            friendsList.add(data.ids[i]); 
+        console.log(friendsList.size());        
     }
     else
     {
@@ -21,18 +24,24 @@ T.get('friends/ids',function(err,data,response){
     }
 })
 
-//Obtaining following list
-// T.get('friends/list',function(err,data,response){
-//     if(!err)
-//     {
-//         console.log(data.users.length);
-//         //Insert code
-//         for(let i=0;i<data.users.length;i++)
-//             list.add(data.users[i].name);
-//             //console.log(data.users[i].name);
-//     }
-//     else
-//     {
-//     console.log(err);
-//     }
-// })
+//Obtaining followers list
+T.get('followers/ids',function(err,data,response){
+    if(!err)
+    {
+        console.log(data.ids.length);
+        //Insert code
+        for(let i=0;i<data.ids.length;i++)
+            followersList.add(data.ids[i]); 
+        console.log("a" + followersList.size());
+    }
+    else
+    {
+    console.log(err);
+    }
+})
+
+unfollowersList.add(followersList.difference(friendsList))
+
+//let unfollowersList = followersList.filter(x => !friendsList.includes(x))
+
+console.log(unfollowersList.size());        
